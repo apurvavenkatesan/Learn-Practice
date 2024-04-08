f1bf83bc3517a68b7d7d13ca09cd6d730d06eac8
Apurva SV <apurvavenkatesan@gmail.com>
2024-04-08 22:08:56 +0530

    Add files via upload

---
+++ JS/Operators/task1.js
@@ -0,0 +1,13 @@
+// Operators - TASK 1
+// Multiply 5 with 7, alerting the result.
+alert(5*7);
+
+//Alert the reminder, once 13 is divided by 7.
+alert(13%7);
+
+
+let x = 10;
+const y=10;
+alert(x+=y); //20
+
+alert(x+=y); //30
---
+++ JS/Operators/task2.js
@@ -0,0 +1,11 @@
+// Operators -Task 2
+var anchoviesPizzaScore = 0;
+var  pepperoniPizzaScore = 4;
+var  hawaiianPizzaScore = 4;
+var  chickenBaconRanchPizzaScore = 6;
+
+console.log("Comparison 1 : anchoviesPizzaScore < pepperoniPizzaScore =", anchoviesPizzaScore < pepperoniPizzaScore);
+console.log("Comparison 2 : chickenBaconRanchPizzaScore > hawaiianPizzaScore =",chickenBaconRanchPizzaScore > hawaiianPizzaScore);
+console.log("Comparison 3 : hawaiianPizzaScore > pepperoniPizzaScore =",hawaiianPizzaScore > pepperoniPizzaScore);
+console.log("Comparison 4 : pepperoniPizzaScore <= chickenBaconRanchPizzaScore =",pepperoniPizzaScore <= chickenBaconRanchPizzaScore);
+console.log("Comparison 5 : hawaiianPizzaScore >= pepperoniPizzaScore =", hawaiianPizzaScore >= pepperoniPizzaScore);
