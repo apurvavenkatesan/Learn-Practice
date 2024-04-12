1990ef00bf7276b61cccf132c4699d46943c9d19
Apurva SV <apurvavenkatesan@gmail.com>
2024-04-12 23:07:53 +0530

    Add files via upload

---
+++ JS/Conditional Statements/task1.js
@@ -0,0 +1,12 @@
+// Controlflow statements and Conditional Statements- TASK1 
+let num1 =3 ;
+let num2 = -7;
+let num3 =2;
+const product= num1*num2*num3;
+if(product>0){
+    alert("The sign is +");
+}else if(product <0){
+    alert("The sign is -");
+}else{
+    alert("The product is 0");
+}
